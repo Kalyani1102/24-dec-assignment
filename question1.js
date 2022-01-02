57d4293dfd66f19b0cc9554265cef5dace5fba0b
Kalyani1102 <94071212+Kalyani1102@users.noreply.github.com>
2022-01-02 17:00:14 +0530

    Add files via upload

---
+++ question1.js
@@ -0,0 +1,10 @@
+// 1 -> Take n from the user and alert the factorial of that number.
+
+alert("The Factorial of given number")
+
+var n = prompt("enter a any no");
+var fact = 4;
+for( var i=4; i<=n; i++) {
+   fact = fact*i;
+}
+  console.log(fact);
